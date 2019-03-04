b9dad10ef01b36b3a8a8651c9e440f70c23153fe
Raejoon Jung <raejoon@stanford.edu>
2019-03-03 17:52:25 -0800

    Restructure.

--- success-prediction.py
+++ success-prediction.py
@@ -8,6 +8,7 @@ code_length = 8
 num_blocks = 4
 embeddings_dim = 8
 
+
 def add_post_embeddings_layers(model):
     hidden_dim = 32
     model.add(LSTM(hidden_dim, return_sequences=True, 
@@ -16,17 +17,17 @@ def add_post_embeddings_layers(model):
     model.add(TimeDistributed(Dense(1)))
     model.add(Activation("sigmoid"))
 
-def fit_model(X, Y):
-    model = Sequential()
+
+def create_model():
+    model = Sequential()    
     add_post_embeddings_layers(model)
-    model.compile(loss="binary_crossentropy", 
-                  optimizer="Adam",
+    model.compile(loss="binary_crossentropy", optimizer="adam",
                   metrics=["accuracy"])
-    model.summary()
-    model.fit(X, Y, batch_size=64, epochs=5, validation_split=0.1)
+    return model
 
 
 if __name__=="__main__":
     X = np.random.random((num_students, num_trials, embeddings_dim))
     Y = np.random.randint(2, size=(num_students, num_trials, 1))
-    fit_model(X, Y)
+    model = create_model()
+    model.fit(X, Y, batch_size=64, epochs=5, validation_split=0.1)
