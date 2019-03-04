22f301673513e179db720b1a5562077bfd4833bf
Raejoon Jung <raejoon@stanford.edu>
2019-03-03 18:26:48 -0800

    Added embedding layer. Keras' embedding layer only take vocabulary indices. Added code_hash function to generate indices for each code (sequence of blocks).

--- success-prediction.py
+++ success-prediction.py
@@ -1,4 +1,6 @@
+import itertools
 import numpy as np
+import matplotlib.pyplot as plt
 from keras.models import Sequential
 from keras.layers import LSTM, Dense, Embedding, TimeDistributed, Activation
 
@@ -8,8 +10,14 @@ code_length = 8
 num_blocks = 4
 embeddings_dim = 8
 
+code_vocab_size = num_blocks ** code_length
 
-def add_post_embeddings_layers(model):
+
+def code_hash(block_array):
+    return np.dot(block_array, num_blocks ** np.arange(code_length))
+
+
+def add_post_embedding_layers(model):
     hidden_dim = 32
     model.add(LSTM(hidden_dim, return_sequences=True, 
                    input_shape=(num_trials, embeddings_dim)))
@@ -18,16 +26,53 @@ def add_post_embeddings_layers(model):
     model.add(Activation("sigmoid"))
 
 
-def create_model():
-    model = Sequential()    
-    add_post_embeddings_layers(model)
+def create_model(embedding_matrix=None):
+    model = Sequential()
+    
+    if embedding_matrix is None:
+        embedding_layer = Embedding(code_vocab_size+1, embeddings_dim, 
+                                    input_length=num_trials)
+    else:
+        embedding_layer = Embedding(code_vocab_size+1, embeddings_dim, 
+                                    input_length=num_trials, trainable=False)
+        embedding_layer.build((None,))
+        embedding_layer.set_weights([embedding_matrix])
+
+    model.add(embedding_layer)
+    add_post_embedding_layers(model)
     model.compile(loss="binary_crossentropy", optimizer="adam",
                   metrics=["accuracy"])
+    model.summary()
     return model
+
+def visualize(history):
+    plt.figure()
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('Model accuracy')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
+    
+    plt.figure()
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Model loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
+    plt.show()
 
 
 if __name__=="__main__":
-    X = np.random.random((num_students, num_trials, embeddings_dim))
+    X = np.random.random((num_students, num_trials, code_length))
+    X_hashed = np.zeros((num_students, num_trials))
+    for i, j in itertools.product(range(num_students), range(num_trials)):
+        X_hashed[i, j] = code_hash(X[i, j, :])
+
     Y = np.random.randint(2, size=(num_students, num_trials, 1))
     model = create_model()
-    model.fit(X, Y, batch_size=64, epochs=5, validation_split=0.1)
+    history = \
+        model.fit(X_hashed, Y, batch_size=64, epochs=5, validation_split=0.1)
+
+    #visualize(history)
