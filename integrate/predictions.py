1fc388176e490ed691cce4b8c39eff6e5aa56b0f
Raejoon Jung <raejoon@stanford.edu>
2019-03-14 21:46:40 -0700

    Add trajectory load function, nn model function, baseline model function, load/save output labels csv function

--- integrate/predictions.py
+++ integrate/predictions.py
@@ -1,6 +1,37 @@
+import os
+import numpy as np
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, Embedding, TimeDistributed, Activation
+
+
+def _get_input_matrix(trajectory_list, traj_to_ast_map, maxlen):
+    X = np.zeros((len(trajectory_list), maxlen))
+    for row_ind, tid in enumerate(trajectory_list):
+        real_part = traj_to_ast_map[tid]
+        appended_part = [real_part[-1]] * (maxlen - len(real_part))
+        X[row_ind,:] = np.array(real_part + appended_part)
+    return X
+
 
 def load_trajectories_from_dataset(trajectory_dirpath):
-    pass
+    idmap_file = os.path.join(trajectory_dirpath, "idMap.txt")
+    # Get trajectory of each student
+    with open(idmap_file) as fo:
+        fo.readline()
+        trajectory_list = [int(line.strip().split(",")[1]) for line in fo]
+        traj_to_ast_map = {tid: [] for tid in set(trajectory_list)}
+    
+    # Get ast list of each trajectory
+    maxlen = 0
+    for tid in traj_to_ast_map:
+        trajectory_file = os.path.join(trajectory_dirpath, str(tid) + ".txt")
+        with open(trajectory_file) as fo: 
+            traj_to_ast_map[tid] = [int(line.strip()) for line in fo]
+            maxlen = max(maxlen, len(traj_to_ast_map[tid]))
+    
+    # Construct matrix
+    return _get_input_matrix(trajectory_list, traj_to_ast_map, maxlen)
+
 
 def load_trajectories_from_file(trajectory_filepath):
     pass
@@ -11,11 +42,69 @@ def save_trajectories_to_file(X, trajectory_filepath):
 def get_output_labels(X):
     pass
 
-def create_baseline_model(X):
-    pass
+def save_output_labels_csv(Y, output_csvpath):
+    with open(output_csvpath, "w") as fo: 
+        for r in range(np.shape(Y)[0]):
+            fo.write(str(r) + "," + ",".join(str(y) for y in Y[r,:]) + "\n");
 
-def create_nn_model(X, embeddings_dict):
-    pass
+def load_output_labels_csv(output_csvpath):
+    with open(output_csvpath) as fo:
+        data = [[int(e) for e in line.strip().split(",")[1:]] for line in fo]
+    
+    return np.array(data)
+
+
+def create_baseline_model(X, num_asts, embeddings_dim, embeddings_matrix=None):
+    num_trials = np.shape(X)[1]
+    model = Sequential() 
+    if embeddings_matrix is None:
+        embedding_layer = Embedding(num_asts+1, embeddings_dim, 
+                                    input_length=num_trials)
+    else:
+        assert(embeddings_dim == np.shape(embeddings_matrix)[1])
+        embedding_layer = Embedding(num_asts+1, embeddings_dim, 
+                                    input_length=num_trials, trainable=False)
+        embedding_layer.build((None,))
+        embedding_layer.set_weights([embeddings_matrix])
+
+    model.add(embedding_layer)
+    model.add(TimeDistributed(Dense(1)))
+    model.add(Activation("sigmoid"))
+    model.compile(loss="binary_crossentropy", optimizer="adam",
+                  metrics=["accuracy"])
+    model.summary()
+    return model
+
+
+def create_nn_model(X, num_asts, embeddings_dim, embeddings_matrix=None):
+    num_trials = np.shape(X)[1]
+
+    model = Sequential()
+
+    if embeddings_matrix is None:
+        embedding_layer = Embedding(num_asts+1, embeddings_dim, 
+                                    input_length=num_trials)
+    else:
+        assert(embeddings_dim == np.shape(embeddings_matrix)[1])
+        embedding_layer = Embedding(num_asts+1, embeddings_dim, 
+                                    input_length=num_trials, trainable=False)
+        embedding_layer.build((None,))
+        embedding_layer.set_weights([embeddings_matrix])
+
+    model.add(embedding_layer)
+    
+    hidden_dim = 32
+    model.add(LSTM(hidden_dim, return_sequences=True, 
+                   input_shape=(num_trials, embeddings_dim)))
+    # TODO: Is there a way to train the dense networks with separate weights?
+    model.add(TimeDistributed(Dense(1)))
+    model.add(Activation("sigmoid"))
+
+    model.compile(loss="binary_crossentropy", optimizer="adam",
+                  metrics=["accuracy"])
+    model.summary()
+    return model
+
 
 def fit_model(model, X, Y):
-    pass
+    model.fit(X, Y, batch_size=64, epochs=5, validation_split=0.1)
---
+++ integrate/test.py
@@ -0,0 +1,83 @@
+import unittest
+import os, shutil
+import numpy as np
+from keras.models import Model
+import predictions
+
+class TestPredictions(unittest.TestCase):
+    def test_load_trajectories_from_dataset(self):
+        os.makedirs("tmp_traj")
+        try:
+            with open("tmp_traj/idMap.txt", "w") as fo:
+                fo.write("secretId,trajectoryId\n")
+                fo.write("0,1\n1,2\n2,2")
+            with open("tmp_traj/1.txt", "w") as fo:
+                fo.write("10\n20")
+            with open("tmp_traj/2.txt", "w") as fo:
+                fo.write("200\n400\n600\n800")
+            X = predictions.load_trajectories_from_dataset("tmp_traj")
+            reference = np.array([[10, 20, 20, 20],
+                                  [200, 400, 600, 800],
+                                  [200, 400, 600, 800]])
+            np.testing.assert_equal(X, reference)
+        except:
+            shutil.rmtree("tmp_traj")
+            raise
+        
+        shutil.rmtree("tmp_traj")
+
+    def test_output_labels_csv(self):
+        filename = "tmp.csv"
+        Y1 = np.array([[0, 1, 1, 0], [1, 0, 1, 0]])
+        predictions.save_output_labels_csv(Y1, filename)
+        Y2 = predictions.load_output_labels_csv(filename)
+        os.remove(filename)
+        np.testing.assert_equal(Y1, Y2)
+
+    def test_create_nn_model_without_embeddings(self):
+        X = np.array([[0, 1, 2, 3], [3, 2, 1, 0]])
+        model = predictions.create_nn_model(X, 4, 2) 
+
+    def test_create_nn_model_with_embeddings(self):
+        # Create model
+        X = np.array([[0, 1, 2, 3], [3, 2, 1, 0]])
+        embeddings_matrix = \
+            np.array([[0., 0.], [0.1, 0.2], [0.2, 0.3], [0.3, 0.2], [0.2, 0.1]])
+        model = predictions.create_nn_model(X, 4, 2, embeddings_matrix) 
+        
+        # Get embedding layer output and make sure it is correct.
+        embedding_layer = Model(inputs=model.input,
+                                outputs=model.layers[0].output)
+        embedding_output = embedding_layer.predict(X)
+        
+        reference = np.zeros((np.shape(X)[0], np.shape(X)[1],
+                              np.shape(embeddings_matrix)[1]))
+        reference[0,:,:] = np.array([[0., 0.], [.1, .2], [.2, .3], [.3, .2]])
+        reference[1,:,:] = np.array([[.3, .2], [.2, .3], [.1, .2], [0., 0.]])
+        np.testing.assert_almost_equal(reference, embedding_output)
+
+    def test_create_baseline_model_without_embeddings(self):
+        X = np.array([[0, 1, 2, 3], [3, 2, 1, 0]])
+        model = predictions.create_baseline_model(X, 4, 2) 
+
+    def test_create_baseline_model_with_embeddings(self):
+        # Create model
+        X = np.array([[0, 1, 2, 3], [3, 2, 1, 0]])
+        embeddings_matrix = \
+            np.array([[0., 0.], [0.1, 0.2], [0.2, 0.3], [0.3, 0.2], [0.2, 0.1]])
+        model = predictions.create_baseline_model(X, 4, 2, embeddings_matrix) 
+        
+        # Get embedding layer output and make sure it is correct.
+        embedding_layer = Model(inputs=model.input,
+                                outputs=model.layers[0].output)
+        embedding_output = embedding_layer.predict(X)
+        
+        reference = np.zeros((np.shape(X)[0], np.shape(X)[1],
+                              np.shape(embeddings_matrix)[1]))
+        reference[0,:,:] = np.array([[0., 0.], [.1, .2], [.2, .3], [.3, .2]])
+        reference[1,:,:] = np.array([[.3, .2], [.2, .3], [.1, .2], [0., 0.]])
+        np.testing.assert_almost_equal(reference, embedding_output)
+        
+
+if __name__=="__main__":
+    unittest.main()
