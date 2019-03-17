0799e17192be87a1dcdc3f9422010a325d5adca4
mathurneeraj <mathur1022@gmail.com>
2019-03-16 18:27:56 -0700

    Save and load model

--- integrate/utils.py
+++ integrate/utils.py
@@ -1,6 +1,17 @@
+from keras.models import load_model
 
-def save_model(model, filepath):
-    pass
+def save_model(model, filepath):    
+    model.save(filepath)
+    
+    # Serialize model to JSON
+    model_json = model.to_json()
+    archpath = filepath + ".json"
+    with open(archpath, "w") as json_file:
+        json_file.write(model_json)        
+    print("Saved model to " + archpath )    
+    
 
-def load_model(filepath):
-    pass
+def load_saved_model(filepath):
+    model = load_model(filepath)
+    print("Loaded model from " + filepath)
+    return model
